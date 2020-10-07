5a8d930e2b7325c84fb8e1b54bb35a841a341bac
Ali <ali.afsah1988@gmail.com>
2020-10-07 12:47:10 +0330

    added work experience

---
+++ components/home-sections/work-experiences/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="work-experience">
+    <div class="container">
+      <div class="header">
+        <h3>What I Did So Far</h3>
+        <p>As a software engineer I was more active in these areas:</p>
+      </div>
+      <div class="experience">
+        <div class="test">
+          <h5>front-end</h5>
+          <p>jfghkdjgbkdfjgh</p>
+        </div>
+        <div class="test">
+          <h5>back-end</h5>
+          <p>zmvbz,jfvjbh</p>
+        </div>
+        <div class="test">
+          <h5>software</h5>
+          <p>lzjfvnl;dfhfnb</p>
+        </div>
+        <div class="test">
+          <h5>engineer</h5>
+          <p>dfkjghdlfjkgh;dkfgh</p>
+        </div>
+        <div class="test">
+          <h5>ali</h5>
+          <p>SDkhglkh</p>
+        </div>
+        <div class="test">
+          <h5>arezoo</h5>
+          <p>fjdhlkgbldkfgh</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="sass">
+@import "./style.scss";
+</style>
